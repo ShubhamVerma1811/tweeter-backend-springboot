c8e570595546e78dda862e9d7cafc9ff89a1d29a
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2024-07-25 10:52:49 +0530

    chore: implement tweet mode

--- tweeter-backend/src/main/java/com/tweeter/tweeter_backend/model/Tweet.java
+++ tweeter-backend/src/main/java/com/tweeter/tweeter_backend/model/Tweet.java
@@ -16,4 +16,55 @@ public class Tweet {
   // TODO:: secondary key
   String authorId;
 
+  public Tweet() {
+  }
+
+  public Tweet(Integer id, String text, String parentTweet, Date createdAt, String authorId) {
+    this.id = id;
+    this.text = text;
+    this.parentTweet = parentTweet;
+    this.createdAt = createdAt;
+    this.authorId = authorId;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public String getParentTweet() {
+    return parentTweet;
+  }
+
+  public void setParentTweet(String parentTweet) {
+    this.parentTweet = parentTweet;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public String getAuthorId() {
+    return authorId;
+  }
+
+  public void setAuthorId(String authorId) {
+    this.authorId = authorId;
+  }
+
 }
